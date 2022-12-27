729ccb0efa2b2997d7741db8f949d6cffb9c3a1c
jonathan <jonathan@cbloom.pro>
2022-12-26 23:09:40 -0500

    adding weather info and position

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirers");
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirers");
 const Busquedas = require("./models/Busquedas");
 
 const main = async()=>{
@@ -15,8 +15,58 @@ const main = async()=>{
 
         switch(opt){
             case 1:
-                const lugar = await leerInput('Ciudad');
-                await busquedas.ciudades(lugar)
+                const termino = await leerInput('Ciudad');
+                const lugares = await busquedas.ciudades(termino)
+                if (lugares.length === 0){
+                    console.log('No se encontraron coincidencias con el lugar ingresado :: '.yellow + termino.red);
+                    break;
+                }
+
+                const id = await listarLugares(lugares);
+
+                if (id === 0){
+                    continue;
+                }
+
+                const lugarSeleccionado = lugares.find( l => l.id === id)
+                console.log(lugarSeleccionado);
+
+                const clima = await busquedas.climaLugar(lugarSeleccionado.lat, lugarSeleccionado.lng)
+
+                console.clear();
+
+                console.log('\nInformacion de la ciudad:\n'.green);
+                console.log('Ciudad: '.yellow + lugarSeleccionado.nombre);
+                console.log('Codigo Corto: '.yellow + ((lugarSeleccionado.short_code === undefined) 
+                                                    ? 'No tiene'
+                                                    : lugarSeleccionado.short_code)
+                                            );
+                
+                console.log('\nLatitud: '.yellow + lugarSeleccionado.lat);
+                console.log('Longitud: '.yellow + lugarSeleccionado.lng);
+
+                const celcius = ' °C'.blue;
+                const grados = ' °'.blue;
+                const metrosSeg = ' mts/s'.blue;
+                const metros = ' mts'.blue;
+                const hPa = ' hPa'.blue;
+                const percentage = ' %'.blue;
+
+                console.log('\nTemperatura:'.green);
+                console.log('Temperatura Actual: '.yellow + clima.main.temp + celcius);
+                console.log('Minima: '.yellow + clima.main.temp_min + celcius);
+                console.log('Maxima: '.yellow + clima.main.temp_max + celcius);
+                
+                console.log('\nMas info:'.green);
+                console.log('Descripcion: '.yellow + clima.weather[0].description);
+                console.log('Presión: '.yellow + clima.main.pressure + hPa);
+                console.log('Humedad: '.yellow + clima.main.humidity + percentage);
+                console.log('Visibilidad: '.yellow + clima.visibility + metros);
+
+                console.log('\nViento:'.green);
+                console.log('Velocidad: '.yellow + clima.wind.speed + metrosSeg);
+                console.log('Direccion del Viento: '.yellow + clima.wind.deg + grados);
+                console.log('Nubosidad: '.yellow + clima.clouds.all + percentage);
 
                 break;
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -8,11 +8,23 @@ class Busquedas{
         
     }
 
+    get language(){
+        return 'es';
+    }
+
     get paramsMapBox(){
         return {
             'access_token': process.env.MAPBOX_KEY,
             'limit': 5,
-            'language':'es'
+            'language': this.language
+        }
+    }
+
+    get paramsOpenWeather(){
+        return {
+            'appid': process.env.OPEN_WEATHER_KEY,
+            'units': 'metric',
+            'lang': this.language
         }
     }
 
@@ -21,16 +33,20 @@ class Busquedas{
 
             const instance = axios.create({
                 baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
-                params: this.paramsMapBox
+                params: this.paramsMapBox,
+                language: 'es',
             })
 
             const resp = await instance.get();
 
-            console.log(instance.baseURL);
-            console.log(resp.data);
+            return resp.data.features.map( e =>({
+                id: e.id,
+                nombre: e.place_name,
+                short_code: e.properties.short_code,
+                lng: e.center[0],
+                lat: e.center[1],
+            }))
 
-            return []
-            
         }catch(error){
             console.log("error");
             console.log(error);
@@ -38,6 +54,29 @@ class Busquedas{
             return [];
         }
 
+    }
+
+    async climaLugar(lat,lon){
+
+        try{
+
+            const instance = axios.create({
+                baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+                params: { ...this.paramsOpenWeather, lat,lon}
+            })
+
+            const resp = await instance.get();
+
+            console.log(resp.data);
+
+            return resp.data
+
+
+        }catch(error){
+            console.log('error');
+            console.log(error);
+            return []
+        }
     }
 
 }
